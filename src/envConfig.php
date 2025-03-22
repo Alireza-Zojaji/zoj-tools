ef570549d5591c528f9207797fd8008d5a0d648a
Alireza Zojaji <zojaji@gmail.com>
2025-03-22 14:13:12 +0330

    108

--- src/envConfig.php
+++ src/envConfig.php
@@ -1,13 +1,24 @@
 <?
 namespace ZojTools;
-
 class envConfig {
-    private $db_host = "";
-    private $db_user="";
-    private $db_password="";
-    private $db_name="";
-    public $db_link;
+    // DB
+    private static $db_host = "";
+    private static $db_user="";
+    private static $db_password="";
+    private static $db_name="";
+    public static $db_link;
     
+    // SMTP
+    public static $smtp_host = "";
+    public static $smtp_user_name = "";
+    public static $smtp_user_password = "";
+    public static $smtp_port = 465;
+    public static $smtp_secure = 'tls';
+    
+    // SMS
+    public static $sms_api_key = '';
+    public static $sms_sender_number='';
+
     public static function init() {
         if (self::$db_link === null) {
             self::$db_link = mysqli_connect(self::$db_host, self::$db_user, self::$db_password, self::$db_name);
@@ -17,15 +28,6 @@ class envConfig {
             }
         }
     }
-    
-    public static $smtp_host = "";
-    public static $smtp_user_name = "";
-    public static $smtp_user_password = "";
-    public static $smtp_port = 465;
-    public static $smtp_secure = 'tls';
-    
-    public static $sms_api_key = '';
-    public static $sms_sender_number='';
 }
 
 ?>
